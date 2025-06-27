9f5b7e80c58874e733f42016871f9a106dbe671c
Diego Santos <diegosantos.engtads@gmail.com>
2025-06-26 22:17:39 -0300

    feat: build SinglePost page using CoverImage and Summary components

--- src/components/SinglePost/index.tsx
+++ src/components/SinglePost/index.tsx
@@ -1,5 +1,7 @@
 import { findPostBySlugCached } from '@/lib/post/queries';
 import { notFound } from 'next/navigation';
+import { PostCoverImage } from '../PostCoverImage';
+import { PostSummary } from '../PostSummary';
 
 type SinglePostProps = {
   slug: string;
@@ -11,8 +13,28 @@ export const SinglePost = async ({ slug }: SinglePostProps) => {
   if (!post) notFound();
 
   return (
-    <div>
-      <p>{post.content}</p>
-    </div>
+    <article>
+      <header>
+        <PostCoverImage
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: post.coverImageUrl,
+            alt: post.title,
+            priority: true,
+          }}
+          className={'rounded-md'}
+        />
+        <PostSummary
+          post={post}
+          className='mt-2 mb-12 cursor-auto'
+          headingAs='h1'
+          disableLink
+        />
+      </header>
+      <section className='bg-background-2 rounded-md p-6'>
+        {post.content}
+      </section>
+    </article>
   );
 };
